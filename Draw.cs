c86e3654086c4528acb5135d9782fb45e283e706
Jia <jiavon@ualberta.ca>
2020-09-03 18:37:33 -0600

    added addshape method

--- Draw.cs
+++ Draw.cs
@@ -19,27 +19,19 @@ namespace C_OOP_Practice_Shape
         //}
         private List<Shapes.Shape> ListShapes { get; set; }// ListShapes (private getter and setter), polymorphic list
 
-        public Draw()    //Draw(Shape), which will accept a polymorphic argument and add that to the ListShapes list.
+        public Draw()
         {
             ListShapes = new List<Shapes.Shape>();
         }
-
         public Draw(Shapes.Shape shape)    //Draw(Shape), which will accept a polymorphic argument and add that to the ListShapes list.
         {
             ListShapes = new List<Shapes.Shape>();
-            ListShapes.Add(shape);
+            AddShape(shape);
         }
 
-        public Draw(int radius)    //Draw(Shape), which will accept a polymorphic argument and add that to the ListShapes list.
+        public void AddShape(Shapes.Shape shape)
         {
-            ListShapes = new List<Shapes.Shape>();
-            ListShapes.Add(new Shapes.Circle(radius));
-        }
-        public Draw(int length, int width)    //Draw(Shape), which will accept a polymorphic argument and add that to the ListShapes list.
-        {
-            ListShapes = new List<Shapes.Shape>();
-            ListShapes.Add(new Shapes.Rectangle(length, width));
-            ListShapes.Add(new Shapes.Triangle(length, width));
+            ListShapes.Add(shape);
         }
 
         //A ToString() override, which will output “A drawing consisting of X shapes.” where X is the number of shapes in the polymorphic list.
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace C_OOP_Practice_Shape
         static void Main(string[] args)
         {
             Draw drawing = new Draw(new Shapes.Circle(3));
+            drawing.AddShape(new Shapes.Triangle(3, 5));
+            drawing.AddShape(new Shapes.Rectangle(3, 5));
+            drawing.AddShape(new Shapes.Triangle(3, 5));
+            drawing.AddShape(new Shapes.Rectangle(3, 5));
             Console.WriteLine(drawing.ToString());
             
         }
